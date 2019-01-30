ca6b574494c79bb49fa18a70ff6a147a6c4718e5
Alejandro Colomina <alejandro.colomina@gmail.com>
2019-01-30 16:38:49 +0100

    Update provider.go

--- src/provider.go
+++ src/provider.go
@@ -7,7 +7,7 @@ import (
 func Provider() *schema.Provider {
 	return &schema.Provider{
 		ResourcesMap: map[string]*schema.Resource{
-      			"example_server": resourceServer(),
-    		},
+			"example_server": resourceServer(),
+		},
 	}
 }
